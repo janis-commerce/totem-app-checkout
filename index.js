e07a29ef68e5c2aeba729d4d0886e6dba47a20a3
alejandro_fizzmod <alejandro@fizzmod.com>
2019-09-11 20:35:44 -0300

    class for management cart vtex

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { Cart } = require('./lib');
+
+module.exports = Cart;
---
+++ tests/totem-app-checkout-test.js
@@ -0,0 +1,15 @@
+/* 'use strict';
+
+const assert = require('assert');
+
+const sandbox = require('sinon').createSandbox();
+
+const TotemAppCheckout = require('./../index');
+
+const TotemAppCheckoutError = require('./../lib/totem-app-checkout-error')
+
+describe('TotemAppCheckout', () => {
+
+
+});
+ */
